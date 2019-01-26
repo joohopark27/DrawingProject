309b7733acbd30eccd60c3f598f0b5e53de0fafb
yottalogical <yottalogical@gmail.com>
2019-01-25 20:41:44 -0500

    Removed unnecessarily overridden methods from Drawing and added Constructors
    Cut out all those overridden methods from Drawing by requiring the client to update manually. This should significantly increase performance, since the DrawingBoard doesn't need to be completely redrawn from scratch every time something is updated. It also means that the client can refresh the DrawingBoard if it ever changes a Drawable inside the Drawing.

--- src/Tester.java
+++ src/Tester.java
@@ -5,6 +5,15 @@ public class Tester {
 		Scanner scan = new Scanner(System.in);
 		Drawing drawing = new Drawing("src/Will.jpg");
 		
-		drawing.add(new Rectangle(0xffffff + 1000, 100, 100, 200, 300));
+		Rectangle rect = new Rectangle(0xffffff + 1000, 100, 100, 200, 300);
+		
+		drawing.add(rect);
+		drawing.refresh();
+		
+		scan.nextLine();
+		
+		drawing.clear();
+		drawing.add(new Rectangle(0x00ff00, 10, 10, 10, 10));
+		drawing.refresh();
 	}
 }
