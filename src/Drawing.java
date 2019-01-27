aba644556f648099c33937cbc66edd8bd1e2d6cb
yottalogical <yottalogical@gmail.com>
2019-01-26 21:44:39 -0500

    Added ability to edit Drawing backgrounds
    Constructors now exist that accept dimensions instead of images.
    
    Setters now exist to change the dimensions or the image, and to change between dimensions and images.

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,11 +1,15 @@
 //TODO Reassign accesses for all classes and methods
+//TODO (At End) Fix all JavaDocs
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class Drawing extends ArrayList<Drawable> {
 	private DrawingBoard drawingBoard;
-	private final String image;
+	private String image;
+	private int width;
+	private int height;
+	private boolean usesImage;
 	
 	/**
 	 * Constructs an empty Drawing with the specified initial capacity and the supplied image.
@@ -18,8 +22,12 @@ public class Drawing extends ArrayList<Drawable> {
 	 */
 	public Drawing(String image, int initialCapacity) {
 		super(initialCapacity);
+		setImage(image);
+	}
+	
+	public void setImage(String image) {
 		this.image = image;
-		this.drawingBoard = new DrawingBoard(image);
+		usesImage = true;
 	}
 	
 	/**
@@ -34,9 +42,10 @@ public class Drawing extends ArrayList<Drawable> {
 	 */
 	public Drawing(String image, Collection<? extends Drawable> c) {
 		super(c);
-		this.image = image;
-		this.drawingBoard = new DrawingBoard(image);
+		setImage(image);
 	}
+	
+	//TODO Create constructors that accept length and width as parameters
 	
 	/**
 	 * Constructs an empty Drawing with an initial capacity of 10 Drawables and the supplied image.
@@ -46,20 +55,67 @@ public class Drawing extends ArrayList<Drawable> {
 	 * @throws NullPointerException if the filepath is invalid
 	 */
 	public Drawing(String image) {
-		this.image = image;
-		this.drawingBoard = new DrawingBoard(image);
+		setImage(image);
 	}
 	
-	//TODO Create constructors that accept length and width as parameters
+	/**
+	 * Constructs an empty list with the specified initial capacity.
+	 *
+	 * @param initialCapacity the initial capacity of the list
+	 *
+	 * @throws IllegalArgumentException if the specified initial capacity is negative
+	 */
+	public Drawing(int width, int height, int initialCapacity) {
+		super(initialCapacity);
+		setDimensions(width, height);
+	}
 	
-	//TODO Create a setImage() method
+	public void setDimensions(int width, int height) {
+		if (height > 0) {
+			this.height = height;
+		} else {
+			this.height = 1;
+		}
+		
+		if (width > 0) {
+			this.width = width;
+		} else {
+			this.width = 1;
+		}
+		
+		usesImage = false;
+	}
+	
+	/**
+	 * Constructs an empty list with an initial capacity of ten.
+	 */
+	public Drawing(int width, int height) {
+		setDimensions(width, height);
+	}
+	
+	/**
+	 * Constructs a list containing the elements of the specified collection, in the order they are returned by the
+	 * collection's iterator.
+	 *
+	 * @param c the collection whose elements are to be placed into this list
+	 *
+	 * @throws NullPointerException if the specified collection is null
+	 */
+	public Drawing(int width, int height, Collection<? extends Drawable> c) {
+		super(c);
+		setDimensions(width, height);
+	}
 	
 	/**
 	 * Updates the DrawingBoard based on the elements in this Drawing.
 	 */
 	public void refresh() {
 		//TODO Make old DrawingBoard disappear when new one appears
-		drawingBoard = new DrawingBoard(image);
+		if (usesImage) {
+			drawingBoard = new DrawingBoard(image);
+		} else {
+			drawingBoard = new DrawingBoard(width, height);
+		}
 		
 		for (Drawable drawable : this) {
 			drawable.drawOn(drawingBoard);
--- src/Tester.java
+++ src/Tester.java
@@ -4,9 +4,7 @@ public class Tester {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Drawing drawing = new Drawing("src/Will.jpg");
-		
 		Rectangle rect = new Rectangle(0x0000ff, 100, 100, 200, 300);
-		
 		drawing.add(rect);
 		drawing.refresh();
 		
@@ -14,6 +12,26 @@ public class Tester {
 		
 		drawing.clear();
 		drawing.add(new Rectangle(0x00ff00, 10, 10, 100, 150));
+		drawing.refresh();
+		
+		scan.nextLine();
+		
+		drawing.setImage("src/Pope.jpg");
+		drawing.refresh();
+		
+		scan.nextLine();
+		
+		drawing.setDimensions(-300, 600);
+		drawing.refresh();
+		
+		scan.nextLine();
+		
+		drawing.setDimensions(600, 300);
+		drawing.refresh();
+		
+		scan.nextLine();
+		
+		drawing.setImage("src/Will.jpg");
 		drawing.refresh();
 	}
 }
